75b1fda3725bfeddd9c6583984635bc99ea54970
James Batsell <jamesbatsell05@gmail.com>
2019-09-18 02:15:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(katz_deli)
-  if katz_deli.empty
+  if katz_deli.empty?
   puts "The line is currently empty."
 end
 end
 
 def line(katz_deli)
-  if katz_deli.empty
+  if katz_deli.empty?
     puts  "The line is currently empty."
   else
     current_line = "The line is currrently:"
